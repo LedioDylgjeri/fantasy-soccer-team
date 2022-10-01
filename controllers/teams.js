03eac99e1b6b2d1823a00d526092678479469f94
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-10-01 15:58:24 -0400

    add new team view

--- controllers/teams.js
+++ controllers/teams.js
@@ -2,11 +2,11 @@ import { Team } from "../models/team.js";
 import { Profile } from "../models/profile.js";
 import { Player } from "../models/player.js";
 
-
-
-
+function newTeam(req, res) {
+  res.render('teams/new')
+}
 
 
 export {
-
+  newTeam as new,
 }
--- routes/teams.js
+++ routes/teams.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import * as teamsCtrl from '../controllers/teams.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
+router.get('/new', isLoggedIn, teamsCtrl.new)
 
 export {
   router
